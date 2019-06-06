bb941a031adaf9cdd7a37ad6da7a47a65e6d4873
Emily <aspentree79@gmail.com>
2019-06-06 20:58:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,17 +6,16 @@ function whisper(hello) {
 return hello.toLowerCase()
 }
 
-var hello = 'hello'
+
 function logShout(string) {
   console.log(hello)
   return (hello).toUpperCase()}
-}
 
-var hello = 'hello'
+
+
 function logWhisper(string) {
   console.log(hello)
-  return `${hello}`
-}
+  return (hello).toLowerCase()}
 
 function sayHiToGrandma(string) {
 if (string === string.toLowerCase()) {return 'I can\'t hear you!';}
