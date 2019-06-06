35dbfa476b7bddb705ada2aaa70f3c540f79ee96
Emily <aspentree79@gmail.com>
2019-06-06 20:53:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,13 +21,6 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
 if (string === string.toLowerCase()) {return 'I can\'t hear you!';}
-}
-
-function sayHiToGrandma(string) {
-if  (string === string.toUpperCase())
-return ('Yes Indeed!').toUpperCase();
-}
-
-function sayHiToGrandma(string){
+if  (string === string.toUpperCase()) {return ('Yes Indeed!').toUpperCase();}
   if (string === 'I love you, Grandma.') {return 'I love you, too.';}
 }
