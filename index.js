3a16de96b4755682c4a6d5f9045103741f56301a
Emily <aspentree79@gmail.com>
2019-06-06 20:48:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ if (string === string.toLowerCase()) {return 'I can\'t hear you!';}
 }
 
 function sayHiToGrandma(string) {
-if  (string === string.toUpperCase()) {return ('Yes indeed!').toUpperCase();}
+if  (string === string.toUpperCase()) {return string.toUpperCase();}
 }
 
 function sayHiToGrandma(string){
