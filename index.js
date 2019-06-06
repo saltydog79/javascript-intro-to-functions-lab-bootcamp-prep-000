8a114a1464218909ebcd1c775e6828487a768f48
Emily <aspentree79@gmail.com>
2019-06-06 20:35:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,8 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
 if (string === string.toLowerCase()) {return 'I can\'t hear you!';}
-else if  (string === string.toUpperCase()) {return 'Yes Ineed!'.toUpperCase();}
+}
+
+function sayHiToGrandma(string) {
+if  (string === string.toUpperCase()) {return 'Yes Ineed!'.toUpperCase;}
 }
