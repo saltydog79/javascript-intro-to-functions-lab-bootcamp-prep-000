d96898ff7538cba578296c832d35dd0d59835cfa
Emily <aspentree79@gmail.com>
2019-06-06 20:05:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(hello) {
+return hello.toUpperCase()
+}
+
+function whisper(hello) {
+return hello.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(hello)
+  return string
+}
+
+logShout('hello')
+
+var hello = 'hello'
+function logWhisper(string) {
+  console.log(hello)
+  return `${hello}`
+}
